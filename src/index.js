8855b7ef88e3952550ffa5b7b426d5dd3f94ffbd
Michael Mathias <michaelscodingjourney@gmail.com>
2023-04-14 12:28:24 -0700

    Created ability to add tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitF(e.target.new_task_description.value);
+  });
 });
+
+function submitF (task) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  p.textContent = `${task}  `;
+  p.appendChild(btn);
+  document.querySelector('#list').appendChild(p);
+}
+
+/*function sortTasks() {
+  const sortTasksSelect = document.getElementById("sort-tasks")
+  if (sortTasksSelect.value === "h-l") {
+     taskArr.sort((a, b) => a.priorityLevel - b.priorityLevel)
+  } else {
+     taskArr.sort((a, b) => b.priorityLevel - a.priorityLevel)
+  }
+  displayTasks()
+}*/
